738fcb1aa679c27cf64ad8e462b692b4d624b487
Derek Weitzel <dweitzel@cse.unl.edu>
2018-05-24 13:57:31 -0500

    Update URL for map

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ class config {
     function __construct() {
         $this->version = "2.0";
         $this->google_analytics_id = "override me";
-        $this->rsvmap = "http://myosg.grid.iu.edu/map/iframe?all_sites=on&active=on&active_value=1&disable_value=1&gridtype=on&gridtype_1=on";
+        $this->rsvmap = "http://map.opensciencegrid.org/map/iframe?all_sites=on&active=on&active_value=1&disable_value=1&gridtype=on&gridtype_1=on";
     }
 }
 
